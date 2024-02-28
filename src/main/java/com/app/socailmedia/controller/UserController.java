683e7889c86ee0e9c6bd453b1fab27b827f5b43e
Farazkhan <farazkhantaraka@gmail.com>
2024-02-28 20:18:24 +0500

    adding and fetching users

--- src/main/java/com/app/socailmedia/controller/UserController.java
+++ src/main/java/com/app/socailmedia/controller/UserController.java
@@ -1,22 +1,38 @@
 package com.app.socailmedia.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.socailmedia.model.User;
+import com.app.socailmedia.services.UserDaoService;
 
 @RestController
 public class UserController {
-    
 
-// what is path variable 
-//  users/path-variable/{name}
-// here the name is a path variable 
-// for that you have to do what 
-@GetMapping("/helloworld/{name}")
-public String helloWorld(@PathVariable String name) {
-    return "hello world"+name;
-}
+    UserDaoService userDaoService;
 
+    // we have three types of dependency injections
+    // constructor dependency injection
+    // setter dependency injections
+    // field dependency injection
+    UserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    // what is path variable
+    // users/path-variable/{name}
+    // here the name is a path variable
+    // for that you have to do what
+    // @GetMapping("/helloworld/{name}")
+    // public String helloWorld(@PathVariable String name) {
+    // return "hello world "+name;
+    // }
+
+    @GetMapping("/users")
+    public List<User> getUser() {
+        return userDaoService.findAllUsers();
+    }
 
 }
--- src/main/java/com/app/socailmedia/model/User.java
+++ src/main/java/com/app/socailmedia/model/User.java
@@ -2,7 +2,14 @@ package com.app.socailmedia.model;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 
+
+@Data
+@AllArgsConstructor
+@ToString
 public class User {
     int id;
     String userName;
