00301f1f290abaeeb57566743721bc150e7a5e3c
Farazkhan <farazkhantaraka@gmail.com>
2024-02-28 19:34:54 +0500

    'first'

---
+++ src/main/java/com/app/socailmedia/SocailmediaApplication.java
@@ -0,0 +1,13 @@
+package com.app.socailmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocailmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocailmediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/socailmedia/SocailmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.socailmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocailmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
